7ee067902ea7a49fbc6745ad6a868ab48dcce895
igorr00 <igor.miskic07@gmail.com>
2022-11-24 14:04:47 +0100

    added material

--- ISA-frontend/src/app/app-routing.module.ts
+++ ISA-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserRegistrationComponent } from './center/user-registration/user-registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'user-registration', component: UserRegistrationComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
