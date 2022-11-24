9476fd0746a0bb1c581640dd5d1b2250c4d8b5e2
igorr00 <igor.miskic07@gmail.com>
2022-11-24 21:19:23 +0100

    asd

--- ISA-frontend/src/app/center/user-profile/user-profile.component.ts
+++ ISA-frontend/src/app/center/user-profile/user-profile.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit {
+  public gender: Number = 0;
+  constructor() { }
 
+  ngOnInit(): void {
+  }
+
+  save(){}
+  cancel(){}
 }
