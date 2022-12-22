0c6e1a7439f277cd58843cbf8cfe8f56c7a46bab
igorr00 <igor.miskic07@gmail.com>
2022-12-22 13:47:25 +0100

    Update data-postgres.sql

--- ISA-backend/src/main/resources/data-postgres.sql
+++ ISA-backend/src/main/resources/data-postgres.sql
@@ -1,7 +1,9 @@
-INSERT INTO public.user (user_id, user_name, user_surname, user_email, user_jmbg, user_password, user_address, user_city, user_country, user_phone, user_gender, user_type, user_occupation, user_company, points, user_category, dtype) VALUES (default, 'Pera', 'Peric', 'pera@gmail.com', '123', 'asd', 'Street Number 1', 'Novi Sad', 'Serbia', '12345', 0, 0, 'none', 'FTN', 0, 0, 'UserRegistered');
+INSERT INTO public.user (user_id, user_name, user_surname, user_email, user_jmbg, user_password, user_address, user_city, user_country, user_phone, user_gender, user_type, user_occupation, user_company, points, user_category, dtype, enabled) VALUES (default, 'Pera', 'Peric', 'pera@gmail.com', '123', 'asd', 'Street Number 1', 'Novi Sad', 'Serbia', '12345', 0, 0, 'none', 'FTN', 0, 0, 'UserRegistered', true);
 
 INSERT INTO public.center (center_id, center_name, center_address, center_description, center_avg_grade) VALUES (default, 'Center One', 'Street Number 1', 'description1', 8);
 INSERT INTO public.center (center_id, center_name, center_address, center_description, center_avg_grade) VALUES (default, 'Center Two', 'Street Number 2', 'description2', 5);
 INSERT INTO public.center (center_id, center_name, center_address, center_description, center_avg_grade) VALUES (default, 'Center Three', 'Street Number 3', 'description3', 7);
 INSERT INTO public.center (center_id, center_name, center_address, center_description, center_avg_grade) VALUES (default, 'Center Four', 'Street Number 4', 'description4', 10);
 INSERT INTO public.center (center_id, center_name, center_address, center_description, center_avg_grade) VALUES (default, 'Center Five', 'Street Number 5', 'description5', 5);
+
+INSERT INTO public.appointment(id, time, center_center_id, user_user_id) VALUES (default, '2023-1-1 11:00', 1, null);
