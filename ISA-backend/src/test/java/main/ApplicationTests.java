86c52f72ec79e17fe2e1fd87553bccf9845c28e1
igorr00 <igor.miskic07@gmail.com>
2023-01-26 20:20:44 +0100

    Update ApplicationTests.java

--- ISA-backend/src/test/java/main/ApplicationTests.java
+++ ISA-backend/src/test/java/main/ApplicationTests.java
@@ -53,7 +53,7 @@ public class ApplicationTests {
 			@Override
 			public void run() {
 		        System.out.println("Startovan Thread 2");
-		        try { Thread.sleep(50); } catch (InterruptedException e) { }// otprilike 100 milisekundi posle prvog threada krece da se izvrsava drugi
+		        try { Thread.sleep(100); } catch (InterruptedException e) { }// otprilike 100 milisekundi posle prvog threada krece da se izvrsava drugi
 		        /*
 		         * Drugi thread pokusava da izvrsi transakcionu metodu findOneById dok se prvo izvrsavanje iz prvog threada jos nije zavrsilo.
 		         * Metoda je oznacena sa NO_WAIT, sto znaci da drugi thread nece cekati da prvi thread zavrsi sa izvrsavanjem metode vec ce odmah dobiti PessimisticLockingFailureException uz poruke u logu:
